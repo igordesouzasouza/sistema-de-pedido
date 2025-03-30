4eb8d65ec3ac62f3eef8466478a1ba0d22195665
Igor de Souza <igordesouzadezembro19@gmail.com>
2025-03-30 10:01:19 -0300

    criando category e files

---
+++ src/controllers/category/CreateCategorieController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+
+class CreateCategorieController {
+   async handle(req: Request, res: Response) {
+    
+   }    
+}
+
+export { CreateCategorieController };
---
+++ src/services/category/CreateCategorieService.ts
@@ -0,0 +1,10 @@
+import prismaClient from "../../prisma";
+
+class CreateCategorieService {
+    async execute(){
+        
+    }
+}
+
+
+export { CreateCategorieService };
