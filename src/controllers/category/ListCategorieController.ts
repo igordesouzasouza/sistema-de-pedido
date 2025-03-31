9fe358540d9d45a42d20f834299013b9999d2516
Igor de Souza <igordesouzadezembro19@gmail.com>
2025-03-31 11:03:57 -0300

    testando

--- src/controllers/category/ListCategorieController.ts
+++ src/controllers/category/ListCategorieController.ts
@@ -1,3 +1,10 @@
 import prismaClient from "../../prisma";
 import { Request, Response } from "express";
 import { ListCategorieService } from "../../services/category/ListCategorieService";
+
+class ListCategorieController {
+    async handle(req: Request, res: Response) {
+        
+    }
+}
+export { ListCategorieController };
--- src/services/category/ListCategorieService.ts
+++ src/services/category/ListCategorieService.ts
@@ -3,7 +3,14 @@ import prismaClient from "../../prisma";
 
 class ListCategorieService {
     async execute() {
-
+        const category = await prismaClient.category.findMany({
+            select: {                
+                id: true,
+                name: true,
+                created_at: true, 
+            },
+        })
+        return category;
     }
 }
 
